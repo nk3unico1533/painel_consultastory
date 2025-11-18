0b20c2941bb22fd704fbe693332120207eb19a9d
nk3unico1533 <nk3unico@outlook.com>
2025-11-18 15:46:31 -0300

    Atualizar o Stories.jsx

--- frontend/src/pages/Stories.jsx
+++ frontend/src/pages/Stories.jsx
@@ -1,9 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/effect-coverflow"; // IMPORT QUE FALTAVA
-
-import { Pagination } from "swiper/modules";
 
 import ConsultaCard from "../components/ConsultaCard";
 
@@ -19,7 +18,7 @@ export default function Stories() {
   ];
 
   return (
-    <div className="h-screen bg-black text-white flex flex-col">
+    <div className="h-screen w-screen bg-black text-white overflow-hidden">
       <Swiper
         modules={[Pagination]}
         pagination={{ clickable: true }}
@@ -28,7 +27,10 @@ export default function Stories() {
         className="h-full"
       >
         {stories.map((s, i) => (
-          <SwiperSlide key={i} className="flex items-center justify-center">
+          <SwiperSlide
+            key={i}
+            className="flex items-center justify-center h-full"
+          >
             <ConsultaCard titulo={s.titulo} rota={s.rota} />
           </SwiperSlide>
         ))}
