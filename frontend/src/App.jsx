3ec5757f968df19dfa460df36f04ce9c5ea3aa45
nk3unico1533 <nk3unico@outlook.com>
2025-11-18 15:04:51 -0300

    Atualizar o App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,5 @@
-import { useEffect, useState } from "react";
-import Stories from "./components/Stories";
-import { getConsultas } from "./api";
+import Stories from "./pages/Stories";
 
 export default function App() {
-  const [consultas, setConsultas] = useState([]);
-
-  useEffect(() => {
-    async function load() {
-      const data = await getConsultas();
-      setConsultas(data);
-    }
-    load();
-  }, []);
-
-  return (
-    <div className="app">
-      <h1>Painel de Consultas</h1>
-      <Stories consultas={consultas} />
-    </div>
-  );
+  return <Stories />;
 }
